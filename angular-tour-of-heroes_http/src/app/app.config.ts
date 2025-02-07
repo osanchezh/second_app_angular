4643e69d3da8c70e49b60acb0884b02e8e9a17a8
Octavio Sánchez Huerta <osanchezhuerta@gmail.com>
2025-02-07 13:10:49 -0600

    agregar proyecto y withdefaults

--- angular-tour-of-heroes_http/src/app/app.config.ts
+++ angular-tour-of-heroes_http/src/app/app.config.ts
@@ -6,15 +6,17 @@ import {routes} from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, withFetch } from '@angular/common/http';
 
 import { importProvidersFrom } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(), 
+  providers: [provideHttpClient(withFetch()), 
     importProvidersFrom([
       HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService)
-    ])]
+    ]),
+    provideRouter(routes), provideClientHydration(), 
+  ]
   //providers: [ provideClientHydration()]
 };
--- angular-tour-of-heroes_http/src/app/hero.service.ts
+++ angular-tour-of-heroes_http/src/app/hero.service.ts
@@ -106,4 +106,17 @@ export class HeroService {
     );
   }
 
+  getHeroNo404<Data>(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/?id=${id}`;
+    return this.http.get<Hero[]>(url)
+      .pipe(
+        map(heroes => heroes[0]), // returns a {0|1} element array
+        tap(h => {
+          const outcome = h ? 'fetched' : 'did not find';
+          this.log(`${outcome} hero id=${id}`);
+        }),
+        catchError(this.handleError<Hero>(`getHero id=${id}`))
+      );
+  }
+
 }
