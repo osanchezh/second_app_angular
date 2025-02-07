9ec8a49aa495ced0649f7e804a05bce8bede7bae
Octavio Sánchez Huerta <osanchezhuerta@gmail.com>
2025-02-07 14:20:35 -0600

    sumit

--- form-driven-app/src/app/app.component.ts
+++ form-driven-app/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
+import { HeroFormComponent } from './hero-form/hero-form.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    CommonModule,
+    FormsModule,
+    HeroFormComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
