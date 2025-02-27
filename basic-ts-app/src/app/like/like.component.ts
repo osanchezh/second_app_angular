7f01af7db4ee50f14549695c3f3555b278e118ed
Octavio Sánchez Huerta <osanchezhuerta@gmail.com>
2025-02-27 17:01:15 -0600

    like component

---
+++ basic-ts-app/src/app/like/like.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-like',
+  standalone: true,
+  imports: [],
+  templateUrl: './like.component.html',
+  styleUrl: './like.component.css'
+})
+export class LikeComponent {
+
+}
