6b96efc2b3d3d56fe893a4cdd3051a79f878f94b
Octavio Sánchez Huerta <osanchezhuerta@gmail.com>
2025-03-21 18:09:24 -0600

    class 8

--- angular17class8/src/app/app.component.ts
+++ angular17class8/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TaskListModule } from './task-list/task-list.module';
+import { TaskListNewSyntaxComponent } from './task-list-new-syntax/task-list-new-syntax.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,TaskListModule],
+  imports: [RouterOutlet,TaskListModule,TaskListNewSyntaxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
